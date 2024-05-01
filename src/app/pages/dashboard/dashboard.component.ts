4fb38986360c527b5a8c96903e4902b2eb7e922f
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-05-01 18:37:13 +0200

    Update dashboard.component.ts

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -518,7 +518,7 @@ if (day >= 10) {
               '#ef8157'
             ],
             borderWidth: 0,
-            data: [this.NumberNewToday, this.NumberIntestedToday,  this.redyNumberToday this.NumberNotIntestedToday,]        
+            data: [this.NumberNewToday, this.NumberIntestedToday,  this.redyNumberToday, this.NumberNotIntestedToday,]        
           
             }]
         },
